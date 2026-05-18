94d4110b2447fbaeed0ad1286bd5d4842534f2a3
Emilio Scott <milii76@outlook.com>
2026-05-18 15:48:55 -0700

    update comparator names for specificity

--- src/BoatLengthComparator.java
+++ src/BoatLengthComparator.java
@@ -1,7 +1,9 @@
 import java.util.Comparator;
 
-public class lengthComparator implements Comparator<Boat> {
+public class BoatLengthComparator implements Comparator<Boat> {
     
+    public BoatLengthComparator(){}
+
     @Override
     public int compare(Boat o1, Boat o2){
         return o1.compareTo(o2);
--- src/BoatPassengerCapacityComparator.java
+++ src/BoatPassengerCapacityComparator.java
@@ -1,6 +1,9 @@
 import java.util.Comparator;
 
-public class passengerCapacityComparator implements Comparator<Boat> {
+public class BoatPassengerCapacityComparator implements Comparator<Boat> {
+    
+    public BoatPassengerCapacityComparator(){}
+
     @Override
     public int compare(Boat o1, Boat o2){
         return o1.getPassengerCapacity() - o2.getPassengerCapacity();
