d82879e3e341cd4717c85d5b885e9088b328628a
Emilio Scott <milii76@outlook.com>
2026-05-18 10:02:39 -0700

    add part of task 2

---
+++ src/lengthComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class lengthComparator implements Comparator<Boat> {
+    
+    @Override
+    public int compare(Boat o1, Boat o2){
+        return o1.getLength() - o2.getLength();
+    }
+}
