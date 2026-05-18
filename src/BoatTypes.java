37fecac43dc27f5b1e77d24bf1254a93f77cd49d
Emilio Scott <milii76@outlook.com>
2026-05-18 14:45:40 -0700

    change ordering of in enum by size

--- src/BoatTypes.java
+++ src/BoatTypes.java
@@ -1,10 +1,10 @@
 public enum BoatTypes {
     CRUISE,        // titanic, disneyShip
     WORK_BOAT,     // tugBoat
-    YACHT,         // yacht (your "Personal Leisure")
+    YACHT, 
+    FERRY,        // yacht (your "Personal Leisure")
     SAILBOAT,
     FISHING_BOAT,
     SPEEDBOAT,
-    FERRY,
-    CANOE
+    CANOE,
 }
