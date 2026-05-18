24d2c4244030b1632e38e59eee7e20ec106552dc
Emilio Scott <milii76@outlook.com>
2026-05-18 14:32:18 -0700

    add test cases for task 1

--- src/BoatTest.java
+++ src/BoatTest.java
@@ -1,4 +1,9 @@
+
 public class BoatTest {
+
+    /**
+     * This class is used for all test cases regarding boat.
+     */
     private static int passed = 0; 
     private static int failed = 0;
 
@@ -6,35 +11,36 @@ public class BoatTest {
         if (condition){passed++;} else { failed++; }
     }
 
+    public static int getPassed(){ return passed; }
+
+    public static int getFailed(){ return failed; }
+
+    
+
     public static void main(String[] args){
+        /**
+         * testing compare to for
+         */
         Boat titanic = new Boat("Cruise", 200, 2000);
-
         Boat disneyShip = new Boat("Cruise", 150, 1500);
-
         Boat tugBoat = new Boat("Work Boat", 20, 5);
-
         Boat yacht = new Boat("Personal Lesuire", 20, 10);
         
         int result1 = titanic.compareTo(tugBoat);
-        
         int result2 = disneyShip.compareTo(titanic);
-
         int result3 = yacht.compareTo(tugBoat);
 
+        boolean checkresult1 = result1 > 0;
+        boolean checkresult2 = result2 < 0;
+        boolean checkresult3 = result3 == 0;
 
-        System.out.println("result1 = testing titanic.compareTo(tugBoat);");
-        System.out.println("Expected result: Positive number");
-        System.out.printf("result: ", result1);
-        System.out.println();
+        check("longer boat is positive", checkresult1 );
+        check("shorter boat is negative", checkresult2);
+        check("boat lengths are equal", checkresult3);
 
-        System.out.println("result2 = testing disneyShip.compareTo(titanic);");
-        System.out.println("Expected Result: negavtive number");
-        System.out.printf("Result: ", result2);
-        System.out.println();
-
-        System.out.println("result3 = testing yacht.compareTo(tugBoat)");
-        System.out.println("expected result: 0");
-        System.out.printf("result: ", result3);
-        System.out.println();
+        System.out.println("Passed tests, " + getPassed() + " | Failed Tests," + getFailed());
+        System.out.println("result1= " + checkresult1);
+        System.out.println("result2= " + checkresult2);
+        System.out.println("result3= " + checkresult3);
     }
 }
