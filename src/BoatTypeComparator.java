0e98cf8cb22bce2151708ed40b7f4435fa28d5e8
Emilio Scott <milii76@outlook.com>
2026-05-18 15:13:48 -0700

    update boatType for accuracy

--- src/BoatTypeComparator.java
+++ src/BoatTypeComparator.java
@@ -1,10 +1,20 @@
 import java.util.Comparator;
 
 public class BoatTypeComparator implements Comparator<Boat>{
+
+    public BoatTypeComparator(){}
     
     @Override
     public int compare(Boat o1, Boat o2){
-        return o1.compareTo(o2);
+        BoatTypes type1 = o1.getType();
+        BoatTypes type2 = o2.getType();
+        
+        /**
+         * Enums automatically implement compareTo, going based off the order the enum. similar to an array.
+         * With the first item in the list being the ord 0, while the last being ord x, increasing for every 
+         * item in the enum.
+         */
+        return type1.compareTo(type2);
     }
 
 }
