5dd26fcaf4718d4f304df735d2e3871eeefd6225
Emilio Scott <milii76@outlook.com>
2026-05-18 15:16:22 -0700

    update tests following update of BoatTypeComparator

--- src/BoatTest.java
+++ src/BoatTest.java
@@ -34,15 +34,41 @@ public class BoatTest {
         boolean checkresult2 = result2 < 0;
         boolean checkresult3 = result3 == 0;
 
+        System.out.println("Boat.compareTo() TESTS");
+
         check("longer boat is positive", checkresult1 );
         check("shorter boat is negative", checkresult2);
         check("boat lengths are equal", checkresult3);
 
-        System.out.println("Passed tests, " + getPassed() + " | Failed Tests, " + getFailed());
         System.out.println("result1= " + checkresult1);
         System.out.println("result2= " + checkresult2);
         System.out.println("result3= " + checkresult3);
-
         
+        /**
+         * generate a comparate instancs so that we can use .compare() method
+         */
+        BoatTypeComparator typeComparator = new BoatTypeComparator();
+
+        System.out.println("BoatTypeComparator TESTS");
+
+        int result4 = typeComparator.compare(titanic, tugBoat);
+        int result5 = typeComparator.compare(disneyShip, titanic);
+        int result6 = typeComparator.compare(yacht, tugBoat);
+
+        boolean checkresult4 = result4 > 0;
+        boolean checkresult5 = result5 == 0;
+        boolean checkresult6 = result6 < 0;
+
+        check("Boat type, titanic compared to tugBoat is positive", checkresult4 );
+        check("Boat type, Titanic and disneyShips are both cruise, so equal.", checkresult5);
+        check("Boat type, yacht compared to workboat is negative.", checkresult6);
+
+        System.out.println("result4= " + checkresult4);
+        System.out.println("result5= " + checkresult5);
+        System.out.println("result6= " + checkresult6);
+
+        System.out.println("BoatTypeComparator TESTS");
+
+        System.out.println("Passed tests, " + getPassed() + " | Failed Tests, " + getFailed());
     }
 }
