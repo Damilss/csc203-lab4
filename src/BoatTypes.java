564a7e7a18dd83773eb77c6cd67a9f8defd2d66e
Emilio Scott <milii76@outlook.com>
2026-05-18 14:38:58 -0700

    add boatTypes enum

---
+++ src/BoatTypes.java
@@ -0,0 +1,10 @@
+public enum BoatTypes {
+    CRUISE,        // titanic, disneyShip
+    WORK_BOAT,     // tugBoat
+    YACHT,         // yacht (your "Personal Leisure")
+    SAILBOAT,
+    FISHING_BOAT,
+    SPEEDBOAT,
+    FERRY,
+    CANOE
+}
